a75f57bf5373b21fd605047ab97713a516a5ce85
etienneplatini <etienne.platini@skycha.com>
2017-05-22 11:23:32 +0200

    cast 0 to unsigned before printf
    Signed-off-by: Frédéric Mahé <mahe@rhrk.uni-kl.de>

--- src/derep.cc
+++ src/derep.cc
@@ -345,7 +345,7 @@ void dereplicate()
           fprint_id_noabundance(statsfile, sp->seqno_first);
           fprintf(statsfile, "\t%lu\t%u\t%u\t%u\n", 
                   db_getabundance(sp->seqno_first),
-                  sp->singletons, 0, 0);
+                  sp->singletons, (unsigned int)0, (unsigned int)0);
           progress_update(i);
         }
       progress_done();
