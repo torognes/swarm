9d56b85935ebe4bc72020a9727715eedaccb4a4b
Frédéric Mahé <frederic.mahe@cirad.fr>
2020-09-30 13:06:22 +0200

    fix warning: implicit conversion changes signedness

--- src/swarm.cc
+++ src/swarm.cc
@@ -418,7 +418,7 @@ void args_init(int argc, char **argv)
 
     if ((c >= 'a') && (c <= 'z'))
       {
-        unsigned int optindex = c - 'a';  // c - 'a' cannot be negative
+        auto optindex = static_cast<unsigned int>(c - 'a');  // c - 'a' cannot be negative
         if (used_options[optindex] == 1)
           {
             int longoptindex {0};
