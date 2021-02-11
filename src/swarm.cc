2aae44a30a9e17de773c7101f18473618a614f55
Frédéric Mahé <frederic.mahe@cirad.fr>
2021-02-11 15:32:29 +0100

    fix a warning when compiling with GCC 4.8.5 and 4.9.4 (2nd try)
    std::array is a class with a C array as its only member. So the outer
    braces initialize the class, and the inner braces default initialize
    the one and only member of the class.

--- src/swarm.cc
+++ src/swarm.cc
@@ -405,7 +405,7 @@ void args_init(int argc, char **argv)
   };
 
   constexpr int n_options {26};
-  std::array<int, n_options> used_options = {};
+  std::array<int, n_options> used_options {{}};
   used_options.fill(0);  // set int values to zero by default
 
   int option_index {0};
