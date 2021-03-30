9588819dce3ff45e5a78c5a8e727282d4d069e0b
Frédéric Mahé <frederic.mahe@cirad.fr>
2021-03-30 16:02:31 +0200

    isolate x86 specific check functions

--- src/swarm.cc
+++ src/swarm.cc
@@ -22,6 +22,7 @@
 */
 
 #include "swarm.h"
+#include "x86_cpu_features.h"
 
 /* OPTIONS */
 
@@ -52,17 +53,6 @@ int64_t penalty_gapopen;
 int64_t penalty_mismatch;
 
 /* Other variables */
-
-int64_t mmx_present {0};
-int64_t sse_present {0};
-int64_t sse2_present {0};
-int64_t sse3_present {0};
-int64_t ssse3_present {0};
-int64_t sse41_present {0};
-int64_t sse42_present {0};
-int64_t popcnt_present {0};
-int64_t avx_present {0};
-int64_t avx2_present {0};
 
 static uint64_t dbsequencecount {0};
 
@@ -144,112 +134,6 @@ const std::vector<std::string> args_usage_message
    "\n"
   };
 
-
-#ifdef __x86_64__
-
-void cpuid(unsigned int f1,
-           unsigned int f2,
-           unsigned int & a,
-           unsigned int & b,
-           unsigned int & c,
-           unsigned int & d);
-
-void cpu_features_detect();
-
-void cpu_features_show();
-
-void cpuid(unsigned int f1,
-           unsigned int f2,
-           unsigned int & a,
-           unsigned int & b,
-           unsigned int & c,
-           unsigned int & d)
-{
-  __asm__ __volatile__ ("cpuid"
-                        : "=a" (a), "=b" (b), "=c" (c), "=d" (d)
-                        : "a" (f1), "c" (f2));
-}
-
-void cpu_features_detect()
-{
-  constexpr unsigned int post_pentium {7};  // new cpus: a & 0xff > 6
-  constexpr unsigned int bit_mmx {23};
-  constexpr unsigned int bit_sse {25};
-  constexpr unsigned int bit_sse2 {26};
-  constexpr unsigned int bit_sse3 {0};
-  constexpr unsigned int bit_ssse3 {9};
-  constexpr unsigned int bit_sse41 {19};
-  constexpr unsigned int bit_sse42 {20};
-  constexpr unsigned int bit_popcnt {23};
-  constexpr unsigned int bit_avx {28};
-
-  unsigned int a {0};
-  unsigned int b {0};
-  unsigned int c {0};
-  unsigned int d {0};
-
-  cpuid(0, 0, a, b, c, d);
-  unsigned int maxlevel = a & UINT8_MAX;
-
-  if (maxlevel >= 1)
-  {
-    cpuid(1, 0, a, b, c, d);
-    mmx_present    = (d >> bit_mmx) & 1;
-    sse_present    = (d >> bit_sse) & 1;
-    sse2_present   = (d >> bit_sse2) & 1;
-    sse3_present   = (c >> bit_sse3) & 1;
-    ssse3_present  = (c >> bit_ssse3) & 1;
-    sse41_present  = (c >> bit_sse41) & 1;
-    sse42_present  = (c >> bit_sse42) & 1;
-    popcnt_present = (c >> bit_popcnt) & 1;
-    avx_present    = (c >> bit_avx) & 1;
-
-    if (maxlevel >= post_pentium)
-    {
-      cpuid(post_pentium, 0, a, b, c, d);
-      constexpr unsigned int bit_avx2 {5};
-      avx2_present   = (b >> bit_avx2) & 1;
-    }
-  }
-}
-
-void cpu_features_show()
-{
-  fprintf(logfile, "CPU features:     ");
-  if (mmx_present != 0){
-    fprintf(logfile, " mmx");
-  }
-  if (sse_present != 0) {
-    fprintf(logfile, " sse");
-  }
-  if (sse2_present != 0) {
-    fprintf(logfile, " sse2");
-  }
-  if (sse3_present != 0) {
-    fprintf(logfile, " sse3");
-  }
-  if (ssse3_present != 0) {
-    fprintf(logfile, " ssse3"); // Supplemental SSSE3, introduced in 2006
-  }
-  if (sse41_present != 0) {
-    fprintf(logfile, " sse4.1");
-  }
-  if (sse42_present != 0) {
-    fprintf(logfile, " sse4.2");
-  }
-  if (popcnt_present != 0) {
-    fprintf(logfile, " popcnt");
-  }
-  if (avx_present != 0) {
-    fprintf(logfile, " avx");
-  }
-  if (avx2_present != 0) {
-    fprintf(logfile, " avx2");
-  }
-  fprintf(logfile, "\n");
-}
-
-#endif
 
 auto args_long(char * str, const char * option) -> int64_t;
 void args_show();
