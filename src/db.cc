5ea6c58c0e01f307896dfee18d365ecd9c1323b9
Frédéric Mahé <frederic.mahe@cirad.fr>
2022-01-14 17:16:14 +0100

    use coma as separator for thousands

--- src/db.cc
+++ src/db.cc
@@ -577,7 +577,7 @@ void db_read(const char * filename, struct Parameters const & p)
 
           /* check length of longest sequence */
           if (length > max_sequence_length) {
-            fatal("Sequences longer than 67 108 861 symbols are not supported.");
+            fatal("Sequences longer than 67,108,861 symbols are not supported.");
           }
 
           linelen = xgetline(& line, & linecap, fp);
