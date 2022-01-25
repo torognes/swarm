65e1ef8d25547283a068f9f2c6e97666d75b0202
Frédéric Mahé <frederic.mahe@cirad.fr>
2022-01-25 19:31:10 +0100

    forgot to add the new nw.h file

---
+++ src/nw.h
@@ -0,0 +1,40 @@
+/*
+    SWARM
+
+    Copyright (C) 2012-2022 Torbjorn Rognes and Frederic Mahe
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Contact: Torbjorn Rognes <torognes@ifi.uio.no>,
+    Department of Informatics, University of Oslo,
+    PO Box 1080 Blindern, NO-0316 Oslo, Norway
+*/
+
+#include <cstdint>
+
+void nw(char * dseq,
+        int64_t dlen,
+        char * qseq,
+        int64_t qlen,
+        int64_t * score_matrix,
+        int64_t gapopen,
+        int64_t gapextend,
+        int64_t * nwscore,
+        int64_t * nwdiff,
+        int64_t * nwalignmentlength,
+        char ** nwalignment,
+        unsigned char * dir,
+        int64_t * hearray,
+        uint64_t queryno,
+        uint64_t dbseqno);
