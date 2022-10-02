3fa4d33a73493d079af1d2781f9cd2cc35b4040f
Frédéric Mahé <frederic.mahe@cirad.fr>
2022-10-02 09:09:50 +0200

    fix spacing

--- src/nw.cc
+++ src/nw.cc
@@ -132,9 +132,9 @@ void nw(char * dseq,
         uint64_t queryno,
         uint64_t dbseqno)
 {
-  /* dir must point to at least qlen*dlen bytes of allocated memory
-     hearray must point to at least 2*qlen longs of allocated memory
-     (8*qlen bytes) */
+  /* dir must point to at least qlen * dlen bytes of allocated memory
+     hearray must point to at least 2 * qlen longs of allocated memory
+     (8 * qlen bytes) */
   constexpr unsigned int multiplier {5};
 
   int64_t n {0};
