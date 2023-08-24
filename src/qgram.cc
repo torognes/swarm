a7311a2dc642d4e71095757162eb49fe3cdeaca1
Frédéric Mahé <frederic.mahe@cirad.fr>
2023-08-24 12:33:56 +0200

    const correctness

--- src/qgram.cc
+++ src/qgram.cc
@@ -115,45 +115,45 @@ auto popcount_128(__m128i x) -> uint64_t
   static constexpr unsigned char m2 {0x33};  // '0011 0011'
   static constexpr unsigned char m4 {0x0f};  // '0000 1111'
 
-  __m128i mask1 = _mm_set_epi8(m1, m1, m1, m1, m1, m1, m1, m1,
-                               m1, m1, m1, m1, m1, m1, m1, m1);
+  const __m128i mask1 = _mm_set_epi8(m1, m1, m1, m1, m1, m1, m1, m1,
+                                     m1, m1, m1, m1, m1, m1, m1, m1);
 
-  __m128i mask2 = _mm_set_epi8(m2, m2, m2, m2, m2, m2, m2, m2,
-                               m2, m2, m2, m2, m2, m2, m2, m2);
+  const __m128i mask2 = _mm_set_epi8(m2, m2, m2, m2, m2, m2, m2, m2,
+                                     m2, m2, m2, m2, m2, m2, m2, m2);
 
-  __m128i mask4 = _mm_set_epi8(m4, m4, m4, m4, m4, m4, m4, m4,
-                               m4, m4, m4, m4, m4, m4, m4, m4);
+  const __m128i mask4 = _mm_set_epi8(m4, m4, m4, m4, m4, m4, m4, m4,
+                                     m4, m4, m4, m4, m4, m4, m4, m4);
 
-  __m128i zero = _mm_setzero_si128();
+  const __m128i zero = _mm_setzero_si128();
 
   /* add together 2 bits: 0+1, 2+3, 3+4, ... 126+127 */
 
-  __m128i a = _mm_srli_epi64(x, 1);
-  __m128i b = _mm_and_si128(x, mask1);
-  __m128i c = _mm_and_si128(a, mask1);
-  __m128i d = _mm_add_epi64(b, c);
+  const __m128i a = _mm_srli_epi64(x, 1);
+  const __m128i b = _mm_and_si128(x, mask1);
+  const __m128i c = _mm_and_si128(a, mask1);
+  const __m128i d = _mm_add_epi64(b, c);
 
   /* add together 4 bits: (0+1)+(2+3), ... (124+125)+(126+127) */
 
-  __m128i e = _mm_srli_epi64(d, 2);
-  __m128i f = _mm_and_si128(d, mask2);
-  __m128i g = _mm_and_si128(e, mask2);
-  __m128i h = _mm_add_epi64(f, g);
+  const __m128i e = _mm_srli_epi64(d, 2);
+  const __m128i f = _mm_and_si128(d, mask2);
+  const __m128i g = _mm_and_si128(e, mask2);
+  const __m128i h = _mm_add_epi64(f, g);
 
   /* add together 8 bits: (0..3)+(4..7), ... (120..123)+(124..127) */
 
-  __m128i i = _mm_srli_epi64(h, 4);
-  __m128i j = _mm_add_epi64(h, i);
-  __m128i k = _mm_and_si128(j, mask4);
+  const __m128i i = _mm_srli_epi64(h, 4);
+  const __m128i j = _mm_add_epi64(h, i);
+  const __m128i k = _mm_and_si128(j, mask4);
 
   /* add together 8 bytes: (0..63) and (64..127) */
 
-  __m128i l = _mm_sad_epu8(k, zero);
+  const __m128i l = _mm_sad_epu8(k, zero);
 
   /* add together 64-bit values into final 128 bit value */
 
-  __m128i m = _mm_srli_si128(l, 8);
-  __m128i n = _mm_add_epi64(m, l);
+  const __m128i m = _mm_srli_si128(l, 8);
+  const __m128i n = _mm_add_epi64(m, l);
 
   /* return low 64 bits: return value is always in range 0 to 128 */
 
