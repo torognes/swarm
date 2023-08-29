b66f1530616fd7439e9b5f6e414ddf6a186f618d
Frédéric Mahé <frederic.mahe@cirad.fr>
2023-08-29 17:14:50 +0200

    struct is constructed with an empty initializer

--- src/db.cc
+++ src/db.cc
@@ -434,7 +434,7 @@ auto db_read(const char * filename, struct Parameters const & parameters) -> voi
 
   /* get file size */
 
-  struct stat fstat_buffer;  // refactor: complex uninitialized struct for fstat() 
+  struct stat fstat_buffer {};
 
   if (fstat(fileno(input_fp), &fstat_buffer) != 0)  // refactor: fstat and fileno linuxisms
     {
