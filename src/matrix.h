de19fa5e6559ad3f8930ed9e82f42a4ea496aae5
Frédéric Mahé <frederic.mahe@cirad.fr>
2023-09-08 15:14:07 +0200

    fix spacing

--- src/matrix.h
+++ src/matrix.h
@@ -32,7 +32,7 @@ constexpr auto n_cells = 32ULL;  // number of chars in sym_nt
 template <typename Integral>
 auto create_score_matrix(const std::int64_t penalty_mismatch)
   -> std::array<Integral, n_cells * n_cells> {
-  static_assert(std::is_same<Integral, unsigned char>::value   \
+  static_assert(std::is_same<Integral, unsigned char>::value \
                 || std::is_same<Integral, unsigned short int>::value \
                 || std::is_same<Integral, std::uint16_t>::value \
                 || std::is_same<Integral, std::int64_t>::value,
