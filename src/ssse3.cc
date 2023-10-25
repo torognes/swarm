d1735bbd9dedaa18040aec7626a49c20e583480b
Frédéric Mahé <frederic.mahe@cirad.fr>
2023-10-25 15:52:08 +0200

    better variable name

--- src/ssse3.cc
+++ src/ssse3.cc
@@ -62,14 +62,15 @@ auto dprofile_shuffle8(BYTE * dprofile,
   const auto seq_chunk3 = _mm_load_si128(dseq_m128i + 3);
 
   auto profline8 = [&](const long long int nuc) {
-    // row: 16 scores from the score matrix, matching the nucleotide 'nuc'
-    // five different nucleotides (0, 1, 2, 3, 4), so five possible rows
-    const auto row = _mm_load_si128(score_matrix_m128i + 2 * nuc);
+    // scores: 16 scores from the score matrix, matching the
+    // nucleotide 'nuc'; five different nucleotides (0, 1, 2, 3, 4),
+    // so five possible rows of scores
+    const auto scores = _mm_load_si128(score_matrix_m128i + 2 * nuc);
 
-    _mm_store_si128(dprofile_m128i + 4 * nuc + 0, _mm_shuffle_epi8(row, seq_chunk0));
-    _mm_store_si128(dprofile_m128i + 4 * nuc + 1, _mm_shuffle_epi8(row, seq_chunk1));
-    _mm_store_si128(dprofile_m128i + 4 * nuc + 2, _mm_shuffle_epi8(row, seq_chunk2));
-    _mm_store_si128(dprofile_m128i + 4 * nuc + 3, _mm_shuffle_epi8(row, seq_chunk3));
+    _mm_store_si128(dprofile_m128i + 4 * nuc + 0, _mm_shuffle_epi8(scores, seq_chunk0));
+    _mm_store_si128(dprofile_m128i + 4 * nuc + 1, _mm_shuffle_epi8(scores, seq_chunk1));
+    _mm_store_si128(dprofile_m128i + 4 * nuc + 2, _mm_shuffle_epi8(scores, seq_chunk2));
+    _mm_store_si128(dprofile_m128i + 4 * nuc + 3, _mm_shuffle_epi8(scores, seq_chunk3));
   };
 
   profline8(0);  // -/gap/no nucleotide (0)
