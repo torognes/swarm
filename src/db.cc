7f441d3b5383d6aa6f47d2da891d280d25242b2e
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-01-08 11:24:49 +0100

    refactoring comment

--- src/db.cc
+++ src/db.cc
@@ -433,7 +433,7 @@ auto db_read(const char * filename, struct Parameters const & parameters) -> voi
     }
 
   /* get file size */
-
+  // refactoring: C++17 std::filesystem::file_size
   struct stat fstat_buffer;  // refactoring: add initializer '{}' (warning with GCC < 5)
 
   if (fstat(fileno(input_fp), &fstat_buffer) != 0)  // refactor: fstat and fileno linuxisms
