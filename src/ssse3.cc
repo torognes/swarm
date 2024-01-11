0a6b1d1731cc4fba61d863841f6e31e1577ed966
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-01-11 11:29:14 +0100

    replace intrinsics with our new free functions

--- src/ssse3.cc
+++ src/ssse3.cc
@@ -70,10 +70,10 @@ auto dprofile_shuffle8(BYTE * dprofile,
     // so five possible rows of scores
     const auto scores = v_load8(score_matrix + 2 * nuc);
 
-    v_store8(dprofile + 4 * nuc + 0, _mm_shuffle_epi8(scores, seq_chunk0));
-    v_store8(dprofile + 4 * nuc + 1, _mm_shuffle_epi8(scores, seq_chunk1));
-    v_store8(dprofile + 4 * nuc + 2, _mm_shuffle_epi8(scores, seq_chunk2));
-    v_store8(dprofile + 4 * nuc + 3, _mm_shuffle_epi8(scores, seq_chunk3));
+    v_store8(dprofile + 4 * nuc + 0, v_shuffle(scores, seq_chunk0));
+    v_store8(dprofile + 4 * nuc + 1, v_shuffle(scores, seq_chunk1));
+    v_store8(dprofile + 4 * nuc + 2, v_shuffle(scores, seq_chunk2));
+    v_store8(dprofile + 4 * nuc + 3, v_shuffle(scores, seq_chunk3));
   };
 
   profline8(0);  // -/gap/no nucleotide (0)
@@ -124,10 +124,10 @@ auto dprofile_shuffle16(WORD * dprofile,
   auto profline16 = [&](const long long int nuc) {
     const auto scores = v_load16(score_matrix + 4 * nuc);
 
-    v_store16(dprofile + 4 * nuc + 0, _mm_shuffle_epi8(scores, m0));
-    v_store16(dprofile + 4 * nuc + 1, _mm_shuffle_epi8(scores, m1));
-    v_store16(dprofile + 4 * nuc + 2, _mm_shuffle_epi8(scores, m2));
-    v_store16(dprofile + 4 * nuc + 3, _mm_shuffle_epi8(scores, m3));
+    v_store16(dprofile + 4 * nuc + 0, v_shuffle(scores, m0));
+    v_store16(dprofile + 4 * nuc + 1, v_shuffle(scores, m1));
+    v_store16(dprofile + 4 * nuc + 2, v_shuffle(scores, m2));
+    v_store16(dprofile + 4 * nuc + 3, v_shuffle(scores, m3));
   };
 
   profline16(0);  // -/gap/no nucleotide (0)
