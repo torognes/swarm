a4deb182d49da352b07b85548b3d2b7573e32499
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-02-05 18:02:51 +0100

    use declared alias

--- src/sse41.cc
+++ src/sse41.cc
@@ -58,7 +58,7 @@ inline auto onestep_16_sse41(VECTORTYPE & H,
                              VECTORTYPE & N,
                              VECTORTYPE & F,
                              VECTORTYPE V,
-                             unsigned short * DIR,
+                             WORD * DIR,
                              VECTORTYPE & E,
                              VECTORTYPE QR,
                              VECTORTYPE R) -> void
@@ -99,7 +99,7 @@ auto align_cells_regular_16_sse41(VECTORTYPE * Sm,
   VECTORTYPE E;
   VECTORTYPE h4;
 
-  auto * const dir = reinterpret_cast<unsigned short *>(dir_long);
+  auto * const dir = reinterpret_cast<WORD *>(dir_long);
 
   const auto Q = *Qm;
   const auto R = *Rm;
@@ -166,7 +166,7 @@ auto align_cells_masked_16_sse41(VECTORTYPE * Sm,
   VECTORTYPE E;
   VECTORTYPE h4;
 
-  auto * const dir = reinterpret_cast<unsigned short *>(dir_long);
+  auto * const dir = reinterpret_cast<WORD *>(dir_long);
 
   const auto Q = *Qm;
   const auto R = *Rm;
