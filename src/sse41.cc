248f4eeaf958943efd57416b4f0de650fbd4070a
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-02-05 18:11:33 +0100

    fix spacing

--- src/sse41.cc
+++ src/sse41.cc
@@ -122,7 +122,7 @@ auto align_cells_regular_16_sse41(VECTORTYPE * Sm,
 
   for(auto i = 0ULL; i < ql; i++)
     {
-      VECTORTYPE *x = qp[i + 0];
+      VECTORTYPE * x = qp[i + 0];
       h4 = hep[2 * i + 0];
       E  = hep[2 * i + 1];
       onestep_16_sse41(h0, h5, f0, x[0], dir + step * i + offset0, E, Q, R);
