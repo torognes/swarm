300dc6158dfd415856ba1dfdf27cf8cc0ff39e82
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-03-04 14:43:28 +0100

    comment

--- src/algo.cc
+++ src/algo.cc
@@ -629,7 +629,8 @@ auto algo_run(struct Parameters const & parameters,
               std::reverse(raw_alignment.begin(), raw_alignment.end());
               compress_alignment_to_cigar(raw_alignment, cigar_string);
 
-              // loosing precision when converting raw_alignment.size() and nwdiff to double is not an issue
+              // loosing precision when converting raw_alignment.size() and
+              // nwdiff to double is not an issue, no need to add assertions
               const auto nwalignmentlength = static_cast<double>(raw_alignment.size());
               const auto differences = static_cast<double>(nwdiff);
               const double percentid = 100.0 * (nwalignmentlength - differences) / nwalignmentlength;
