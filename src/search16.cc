3b2a5f97a590c93c758f61c3081a60e290240a49
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-03-07 14:50:01 +0100

    switch to signed variables

--- src/search16.cc
+++ src/search16.cc
@@ -235,8 +235,8 @@ auto align_cells_regular_16(VECTORTYPE * Sm,
                             uint64_t * dir_long,
                             VECTORTYPE * H0) -> void
 {
-  static constexpr auto step = 16U;
-  static constexpr auto offset0 = 0U;
+  static constexpr auto step = 16;
+  static constexpr auto offset0 = 0;
   static constexpr auto offset1 = offset0 + 4;
   static constexpr auto offset2 = offset1 + 4;
   static constexpr auto offset3 = offset2 + 4;
@@ -267,7 +267,8 @@ auto align_cells_regular_16(VECTORTYPE * Sm,
   assert(ql <= max_ptrdiff);
   assert(ql <= ((max_ptrdiff - 1) / 2));  // max 'E' offset
   assert(ql <= ((max_ptrdiff - offset3) / step));  // max 'dir' offset
-  for(auto pos = 0ULL; pos < ql; ++pos)
+  auto const ql_signed = static_cast<std::ptrdiff_t>(ql);
+  for(auto pos = 0LL; pos < ql_signed; ++pos)
     {
       VECTORTYPE *x = qp[pos + 0];
       h4 = hep[2 * pos + 0];
