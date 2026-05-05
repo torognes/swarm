35186cc92de241ef4277de42961538680c0d529a
Frédéric Mahé <frederic.mahe@cirad.fr>
2026-05-05 11:46:23 +0200

    spacing

--- src/swarm.cc
+++ src/swarm.cc
@@ -1,24 +1,24 @@
 /*
-    SWARM
+  SWARM
 
-    Copyright (C) 2012-2026 Torbjorn Rognes and Frederic Mahe
+  Copyright (C) 2012-2026 Torbjorn Rognes and Frederic Mahe
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    Contact: Torbjorn Rognes <torognes@ifi.uio.no>,
-    Department of Informatics, University of Oslo,
-    PO Box 1080 Blindern, NO-0316 Oslo, Norway
+  Contact: Torbjorn Rognes <torognes@ifi.uio.no>,
+  Department of Informatics, University of Oslo,
+  PO Box 1080 Blindern, NO-0316 Oslo, Norway
 */
 
 #include "swarm.h"  // struct Parameters
@@ -29,8 +29,7 @@
 #include "derep.h"
 
 
-auto main(int argc, char** argv) -> int
-{
+auto main(int argc, char ** argv) -> int {
   // initialization and checks
   auto const parameters = parse_command_line(argc, argv);
 
@@ -38,18 +37,17 @@ auto main(int argc, char** argv) -> int
   Data const data {parameters};
 
   // clustering
-  switch (parameters.opt_differences)
-    {
-    case 0:
-      dereplicate(parameters, data);
-      break;
+  switch (parameters.opt_differences) {
+  case 0:
+    dereplicate(parameters, data);
+    break;
 
-    case 1:
-      algo_d1_run(parameters, data);
-      break;
+  case 1:
+    algo_d1_run(parameters, data);
+    break;
 
-    default:
-      algo_run(parameters, data);
-      break;
-    }
+  default:
+    algo_run(parameters, data);
+    break;
+  }
 }
