ee088e64a673995eac0d37a952d3d5c8ae82cdb4
Frédéric Mahé <frederic.mahe@cirad.fr>
2026-05-05 12:12:02 +0200

    spacing

--- src/cli.cc
+++ src/cli.cc
@@ -638,7 +638,6 @@ namespace {
   auto check_scoring_saturation(struct Parameters const & parameters) -> void {
     static constexpr auto uint8_max = std::numeric_limits<uint8_t>::max();
     static constexpr auto uint16_max = std::numeric_limits<uint16_t>::max();
-
     int64_t const diff_saturation_16 = std::min((uint16_max / parameters.penalty_mismatch),
                                                 (uint16_max - parameters.penalty_gapopen)
                                                 / parameters.penalty_gapextend);
